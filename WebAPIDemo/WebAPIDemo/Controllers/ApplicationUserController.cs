7a9989e5f84bce37d9b41c3aeac027918cac6377
dathochoi <dathochoi01@yahoo.com.vn>
2020-03-24 17:21:39 +0700

    saas

---
+++ WebAPIDemo/WebAPIDemo/Controllers/ApplicationUserController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+      //  private readonly ApplicationSettings _appSetting.value;
+
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager/*, ApplicationSettings appSetting*/)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+           // _appSetting = appSetting;
+        }
+
+        [HttpGet]
+        [Route("GetVer")]
+        public Object GetVerstion()
+        {
+            return Ok("dan ket noi");
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var tokenDescripTor = new SecurityTokenDescriptor
+                {
+                    Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                    {
+                            new Claim("UserID", user.Id.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("123456789054623463")), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var tokeHandler = new JwtSecurityTokenHandler();
+                var securityToken = tokeHandler.CreateToken(tokenDescripTor);
+                var token = tokeHandler.WriteToken(securityToken);
+
+                return Ok(new { token });
+            }
+            else
+                return BadRequest(new { message = "Username or password is incorrect!!!" });
+        }
+    }
+}
---
+++ appLogin/src/app/shared/user.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { FormBuilder, Validators, FormGroup} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+   readonly baseURI = 'https://localhost:44323/api';
+   //readonly baseURI = 'https://localhost:54277/api';
+ // readonly baseURI = 'https://localhost:44359/api/en/Product';
+
+
+  formModel = this.fb.group({
+    UserName: ['', Validators.required],
+    Email: ['', Validators.email],
+    FullName: [''],
+    Passwords: this.fb.group({
+      Password: ['', [Validators.required, Validators.minLength(4)]],
+      ConfirmPassword: ['', Validators.required]
+    }, { validator: this.comparePasswords })
+
+  });
+
+  comparePasswords(fb: FormGroup) {
+    let confirmPswrdCtrl = fb.get('ConfirmPassword');
+    //confirmPswrdCtrl.errors={passwordMismatch:true}
+    if (confirmPswrdCtrl.errors == null || 'passwordMismatch' in confirmPswrdCtrl.errors) {
+      if (fb.get('Password').value != confirmPswrdCtrl.value)
+        confirmPswrdCtrl.setErrors({ passwordMismatch: true });
+      else
+        confirmPswrdCtrl.setErrors(null);
+    }
+  }
+
+  register() {
+    var body = {
+      UserName: this.formModel.value.UserName,
+      Email: this.formModel.value.Email,
+      FullName: this.formModel.value.FullName,
+      Password: this.formModel.value.Passwords.Password
+    };
+    return this.http.post(this.baseURI + '/ApplicationUser/Register', body);
+  }
+  login(formData) {
+    return this.http.post(this.baseURI + '/ApplicationUser/Login', formData);
+  }
+
+  getUserProfile() {
+    return this.http.get(this.baseURI + '/UserProfile');
+  }
+
+}
